aa5690e94512f60661c0236fa01eb63b200d5164
kennylui <35885529+kennylui@users.noreply.github.com>
2019-09-03 18:00:52 +0800

    Update funnymain.js

--- funnymain.js
+++ funnymain.js
@@ -3,13 +3,13 @@ function newGame(){
   this.attack = 5;
   this.defense = 5;
   this.money = 0;
-  document.write(Health: this.hp);
+  document.write("Health: "+this.hp);
   document.write("\n");
-  document.write(Attack: this.attack);
+  document.write("Attack: "+this.attack);
   document.write("\n");
-  document.write(Defense: this.defense);
+  document.write("Defense: "+this.defense);
   document.write("\n");
-  document.write(Money: this.money);
+  document.write("Money: "+this.money);
 }
 
 function main(){

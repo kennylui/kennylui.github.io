7e6fc4f2cae6e8fe6849826665f8a46fba5e6eb0
kennylui <35885529+kennylui@users.noreply.github.com>
2019-09-03 18:29:04 +0800

    Update intial_menu.js

--- intial_menu.js
+++ intial_menu.js
@@ -1,1 +1,19 @@
+function newGame(){
+  this.hp = 20;
+  this.attack = 5;
+  this.defense = 5;
+  this.money = 0;
+  document.write("Health: "+this.hp);
+  document.write("<br>");
+  document.write("Attack: "+this.attack);
+  document.write("<br>");
+  document.write("Defense: "+this.defense);
+  document.write("<br>");
+  document.write("Money: "+this.money);
+  document.write("<br>");
+}
 
+function main(){
+  //resetgame
+  newGame();
+}

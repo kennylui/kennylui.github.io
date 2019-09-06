ba62324c41555b796451a6e5443d4b4f377dd070
kennylui <35885529+kennylui@users.noreply.github.com>
2019-09-06 16:54:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -60,6 +60,22 @@ function drawNumber() {
     }
 }
 
+function prev(){
+  clearDisplay();
+  Display();
+}
+
+function next(){
+  clearDisplay();
+  if(0<current.position<11){
+    current.monthposition++;
+  }
+  else if(current.position == 11)(){
+    current.monthposition = 0;
+  }
+  Display(current.monthposition, current.year);
+}
+
 function Display(currentmonth, currentyear) {
     drawGrid();
     drawWeek();

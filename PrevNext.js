797d32b787f86e0f1c8ac78537838123eb6f6f85
kennylui <35885529+kennylui@users.noreply.github.com>
2019-09-06 16:41:22 +0800

    Update PrevNext.js

--- PrevNext.js
+++ PrevNext.js
@@ -5,6 +5,11 @@ function prev(){
 
 function next(){
   clearDisplay();
-  current.monthposition++;
+  if(0<current.position<11){
+    current.monthposition++;
+  }
+  else if(current.position == 11)(){
+    current.monthposition = 0;
+  }
   Display(current.monthposition, current.year);
 }

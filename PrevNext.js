1839f74dcc1f147efb9c1521c382eac873a90d80
kennylui <35885529+kennylui@users.noreply.github.com>
2019-09-06 17:29:52 +0800

    Update PrevNext.js

--- PrevNext.js
+++ PrevNext.js
@@ -3,11 +3,9 @@ function previous(){
   if(0<current.position<=11){
     current.monthposition--;
   }
-  else if(current.position == 0){
-    current.monthposition = 11;
-  }
-  else{}
-  Display();
+  else
+  {current.monthposition = 11;}
+  Display(current.monthposition, current.year);
 }
 
 function next(){
@@ -15,10 +13,9 @@ function next(){
   if(0<=current.position<11){
     current.monthposition++;
   }
-  else if(current.position == 11){
-    current.monthposition = 0;
-  }
-  else{}
+  else
+  {current.monthposition = 0;}
+  
   Display(current.monthposition, current.year);
 }
 
